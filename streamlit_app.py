7ed206c8db1fbabf2c75064baac920f03ca93ace
SiyabongaThwala <59641479+SiyabongaThwala@users.noreply.github.com>
2023-10-19 10:42:53 +0200

    Update streamlit_app.py
    show API data in the table view

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,11 @@ streamlit.dataframe(fruits_to_show)
 #New Section to Discplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+#streamlit.text(fruityvice_response.json())
+
+# normalize the json output to look readable
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# show the normalized data
+streamlit.dataframe(fruityvice_normalized)
